d3204153f00198877ff56256f819330ec4f73ada
Le Huynh <lhuynh100@gmail.com>
2022-09-08 17:40:15 -0500

    commit tree 78d6083bf14dbba19fec78cfa2f60b653a9cd010

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+} 
+return messages;
+}
+console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], 'birthday'));
+
+
+function countDown(i) {
+    let countDown = 0;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
